0336701a981cb3f6332a7d8bb41b1e096ba0372d
waiwai1000 <41092236+waiwai1000@users.noreply.github.com>
2019-11-26 17:03:45 +0800

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -192,15 +192,7 @@ confirm("Are you sure to delete?")
     });
     
   }
-   generateRandomString(length) {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-     
-    for (var i = 0; i < length; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-     
-    return text;
-  }
+   
   
   register_key() {
     var text = "";
